a05501996ec52bcf12f8b3ee5886ca65a4ffa43c
Alessandro Martini <alessandro.martini@loadsmart.com>
2021-07-23 21:51:33 -0300

    test(utils): add tests to parseSettings

---
+++ tests/utils/settings.spec.ts
@@ -0,0 +1,33 @@
+import { SettingsValue } from '@/context/provider';
+import { parseSettings } from '@/utils/settings';
+
+describe('parseSettings', () => {
+  const settings: SettingsValue = {
+    flags: {
+      AAA: true,
+      BBB: false,
+      CCC: true,
+    },
+    settings: {
+      DDD: 'foo',
+      EEE: 'bar',
+      FFF: 'baz',
+    },
+  };
+
+  it('extracts the values from settings', () => {
+    expect(parseSettings(['flags.AAA', 'settings.EEE'], settings)).toStrictEqual([
+      settings.flags.AAA,
+      settings.settings.EEE,
+    ]);
+  });
+
+  it('returns null for missing value', () => {
+    expect(
+      parseSettings(
+        ['flags.AAA', 'settings.EEE', 'this is a missing value', 'flags.MISSING', 'settings.NOT_HERE'],
+        settings,
+      ),
+    ).toStrictEqual([settings.flags.AAA, settings.settings.EEE, null, null, null]);
+  });
+});
